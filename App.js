6dff9f50a4385a42d76be468594818784c106b64
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-11-07 06:59:51 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// import react Navigation
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+
+// import the screens
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+// Create the navigator
+const navigator = createStackNavigator({
+  Start: { screen: Start },
+  Chat: { screen: Chat }
+});
+
+const navigatorContainer = createAppContainer(navigator);
+// Export it as the root component
+export default navigatorContainer;
